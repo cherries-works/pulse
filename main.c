466dfeff55b0704241b41befbb0a4ba0eaf7d08e
= <mertkucukhuseyin90@gmail.com>
2026-06-26 00:18:58 +0200

    Running a proper exit on SIGINT to close all processes, and stop truthy loops (#5).

--- main.c
+++ main.c
@@ -4,107 +4,93 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <signal.h>
+#include <wait.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 
 #include "parse.h"
 #include "render.h"
 #include "utils.h"
 #include "http.h"
+#include "daemon.h"
 
 #include "app.h"
+#include "setup.h"
 
-typedef struct PulseArgs {
-    bool web;
-    bool help;
-    
-    int port;
-    unsigned sleep;
-} PulseArgs;
-
-PulseArgs parseArgs(int argc, char* argv[]) {
-    PulseArgs p = {
-        .web = false,
-        .help = false,
-        .sleep = 1,
-        .port = 8080,
-    };
-
-    for(int i = 0; i < argc; i++) {
-        char* arg = argv[i];
-
-        if(strcmp(arg, "--sleep") == 0) {
-            if(i == argc - 1) continue;
-            unsigned sleep = (unsigned)atoi(argv[i + 1]);
-            if(sleep <= 0 || sleep > 60) continue;
-            p.sleep = sleep;
-        }
-
-        if(strcmp(arg, "--port") == 0) {
-            if(i == argc - 1) continue;
-            int port = atoi(argv[i + 1]);
-            p.web = 1;
-            p.port = port;
-        }
-
-        if(strcmp(arg, "--web") == 0) {
-            p.web = true;
-        }
-
-        if(strcmp(arg, "--help") == 0) {
-            p.help = true;
-            break;
-        }
+void stop() {
+    char *home = getenv("HOME");
+    if(home == NULL) {
+        printf("Error: No HOME enviroment variable...");
+        return;
     }
 
+    char path_dir[512];
+    sprintf(path_dir, "%s/%s", home, R_CHERRIES_FOLDER_PULSE);
+    mkdir(path_dir, 0755);
 
-    return p;
+    char file_path[1028];
+
+
+    DIR *dir = opendir(path_dir);
+    struct dirent *entry;
+    while((entry = readdir(dir)) != NULL) {
+        char *name = entry->d_name;
+        if(strcmp(name, ".") == 0) continue;
+        if(strcmp(name, "..") == 0) continue;
+        sprintf(file_path, "%s/%s", path_dir, name);
+
+        char *d = strchr(name, '.');
+        if(d == NULL) continue;
+        name = d + 1;
+        int pid = atoi(name);
+        kill(pid, SIGKILL);
+        waitpid(pid, NULL, 0);
+        
+        remove(file_path);
+    }
+
+    closedir(dir);
 }
 
+void term(int sig) {
+    running = 0;
+    stop();
+    exit(0);
+}
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {    
+    signal(SIGTERM, term);
+    int s = setup();
+    if(s < 0) {
+        printf("Failed to run setup.\n");
+        return -1;
+    }
+
     PulseArgs args = parseArgs(argc, argv);
     if(args.help) {
         help();
         return 0;
     }
+
+    if(args.stop) {
+        stop();
+        return 0;
+    }
     
-    System systemSnapshot = getSystem();
-    System prevSystemSnapshot = systemSnapshot;
+    startDaemon(args);
+    if(args.headless && !args.web) return 0;
 
+    sleep(1); // to prevent shm_open failing 
     if(args.web) {
-        printf("API running on :: %d\n", args.port);
-        Route routes[64];
-        RouteHandler handler = { 0, routes };
-        initRoutes(&handler);
-
-        Server server = serverContsructor(
-            &handler,           // route handler
-            AF_INET, 
-            (uint16_t)args.port,
-            SOCK_STREAM,
-            0,                  // protocol
-            10,                 // backlog
-            INADDR_ANY,
-            serverLaunch
-        );
-        
-        pthread_t serverThread;
-        pthread_create(&serverThread, NULL, serverLaunchThread, &server);
+        startWebsite(args);
     }
 
-    bool LOOP_STARTED = false;
-    short TOTAL_LINES = 15;
-    while(true) {
-        if(!LOOP_STARTED) {
-            LOOP_STARTED = true;
-        } else {
-            clearLines(TOTAL_LINES);
-        }
-        
-        prevSystemSnapshot = systemSnapshot;
-        systemSnapshot = getSystem();
-
-        render(systemSnapshot, prevSystemSnapshot);
-        sleep(args.sleep);
+    if(!args.headless) {
+        startRender(args);
     }
 
     return 0;
