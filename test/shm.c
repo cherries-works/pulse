38431a236fc48f1060308074cea6ff5684fc17df
= <mertkucukhuseyin90@gmail.com>
2026-06-30 02:28:28 +0200

    Updated tests with mutex lock (#9).

--- test/shm.c
+++ test/shm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <pthread.h>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/un.h>
@@ -21,19 +22,9 @@ void testReadDaemonS() {
         exit(EXIT_FAILURE);
     }
 
+    pthread_mutex_lock(&shmp->lock);
     memcpy(&system, &shmp->system, sizeof(System));
-    /* Tell peer that it can now access shared memory.  */
-    if (sem_post(&shmp->sem1) == -1) {
-        printf("Posting access to modifieable data failed (sem 1).\n");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Wait until peer says that it has finished accessing
-        the shared memory.  */
-    if (sem_wait(&shmp->sem2) == -1) {
-        printf("Posting access finished failed (sem 2).\n");
-        exit(EXIT_FAILURE);
-    }
+    pthread_mutex_unlock(&shmp->lock);
 
     printf("%lld\n", system.cpu.idle);
     printf("%lld\n", system.disk.available);
@@ -54,19 +45,9 @@ void testReadDaemonM() {
         exit(EXIT_FAILURE);
     }
 
+    pthread_mutex_lock(&shmp->lock);
     memcpy(&metrics, &shmp->metrics, sizeof(Metrics));
-    /* Tell peer that it can now access shared memory.  */
-    if (sem_post(&shmp->sem1) == -1) {
-        printf("Posting access to modifieable data failed (sem 1).\n");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Wait until peer says that it has finished accessing
-        the shared memory.  */
-    if (sem_wait(&shmp->sem2) == -1) {
-        printf("Posting access finished failed (sem 2).\n");
-        exit(EXIT_FAILURE);
-    }
+    pthread_mutex_unlock(&shmp->lock);
 
     printf("%f\n", metrics.cpuUsage);
     printf("%f\n", metrics.diskUsage);
