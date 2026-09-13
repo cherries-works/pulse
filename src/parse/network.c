e886e1b36230225e4c302421ccba22d2b7135c6d
= <mertkucukhuseyin90@gmail.com>
2026-09-13 17:48:43 +0200

    Improved getNetwork function.

--- src/parse/network.c
+++ src/parse/network.c
@@ -9,69 +9,74 @@ Network getNetwork(size_t size, char *buffer) {
     unsigned long tx = 0;
 
     char *line = buffer;
-    char *next = strchr(line, '\n');
-    if(next) {
-        line = next + 1;
-        next = strchr(line, '\n');
-        if(next) {
+    for (int i = 0; i < 2; i++) {
+        char *next = strchr(line, '\n');
+
+        if (next) {
             line = next + 1;
         } else {
-            line = line + strlen(line);
+            Network network = { 0, 0 };
+            return network;
         }
-    } else {
-        line = line + strlen(line);
     }
-    
+
     // Inter-|   Receive           |  Transmit                          \n <--- first strchr
     // face  |bytes  ..  multicast|bytes    packets .... compressed     \n <--- second strchr
     //                                                                  So we start at the start of lo
     // lo:   38914   ..  38914     307    0    ....                0
     // enp2s0:    0 ....         0        0       ....               0
     // wlp0s20f3: 183282384 ....         0 18357363   ....       0          0
+    int KEYS_AMOUNT = 8;
+    while (*line) {
+        char *colon = strchr(line, ':');
+        if (!colon) break;
 
-    next = strchr(line, ':');
-    if(next) line = next + 1;
-    else line = line + strlen(line);
+        line = colon + 1;
 
-    while(*line) {
-        // 8 total keys per face (for rx AND tx)
-        int KEYS_AMOUNT = 8;
-        for(int i = 0; i < KEYS_AMOUNT; i++) {
+        for (int i = 0; i < KEYS_AMOUNT; i++) {
             while (*line == ' ') line++;
 
-            next = strchr(line, ' ');
-            if(next) *next = '\0';
-            else continue;
+            char *next = strchr(line, ' ');
 
-            rx += strtoul(line, NULL, 10);
-            if(next) line = next + 1;
-            else line = line + strlen(line);
+            if (next) *next = '\0';
+
+            if (i == 0) rx += strtoul(line, NULL, 10);
+            if (next) {
+                line = next + 1;
+            } else {
+                break;
+            }
         }
 
-        for(int i = 0; i < KEYS_AMOUNT; i++) {
+        for (int i = 0; i < KEYS_AMOUNT; i++) {
             while (*line == ' ') line++;
+            char *next = strchr(line, ' ');
 
-            next = strchr(line, ' ');
-            if(next) *next = '\0';
-            else continue;
+            if (next) *next = '\0';
 
-            tx += strtoul(line, NULL, 10);
-            
-            if(next) line = next + 1;
-            else line = line + strlen(line);
+            if (i == 0) tx += strtoul(line, NULL, 10);
+
+            if (next) {
+                line = next + 1;
+            } else {
+                break;
+            }
         }
 
-        next = strchr(line, '\n');
-        if(next) line = next + 1;
-        else line = line + strlen(line);
+        // Move to the next interface.
+        char *next = strchr(line, '\n');
+
+        if (next) {
+            line = next + 1;
+        } else {
+            break;
+        }
     }
 
-
     Network snapshot = { rx, tx };
+
     return snapshot;
 }
-
-
 
 NetworkAverage parseNetworkUsage(Network snapshot2, Network snapshot1) {
     float rx = (float)snapshot2.rx - (float)snapshot1.rx;
