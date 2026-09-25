fac6d7845cd7d26e2f9e1413008540405be30fb6
= <mertkucukhuseyin90@gmail.com>
2026-09-26 00:30:55 +0200

    Added command completions for args as well (#35).

--- contrib/completions/pulse.bash
+++ contrib/completions/pulse.bash
@@ -1,15 +1,54 @@
 _pulse() {
-    local cur prev commands
-
+    local cur 
     cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    local command 
+    command="${COMP_WORDS[1]}"
 
-    commands="monitor info top stop config prune help snapshot version process"
+    local options 
+    local word
 
+    # if there is not a command set yet print the commands first
     if [[ $COMP_CWORD -eq 1 ]]; then
+        local commands="monitor info top stop config prune help snapshot process version"
         COMPREPLY=( $(compgen -W "$commands" -- "$cur") )
         return
     fi
+
+    case "$command" in
+        monitor)
+            options="--port --web --sleep --headless --processes --sort"
+            ;;
+        info|snapshot)
+            options="--json"
+            ;;
+        top)
+            options="--processes --sort"
+            ;;
+        process)
+            options="--process"
+            ;;
+        prune)
+            options="--keep --until --prune"
+            ;;
+        config)
+            options="--reset"
+            ;;
+        *)
+            return
+            ;;
+    esac
+
+    for word in "${COMP_WORDS[@]:2}"; do
+        # if the argument is already passed in, then dont re-suggest it
+        case "$word" in
+            --port|--web|--sleep|--headless|--processes|--sort|\
+            --json|--process|--keep|--until|--prune|--reset)
+                options="${options//$word/}"
+                ;;
+        esac
+    done
+
+    COMPREPLY=($(compgen -W "$options" -- "$cur"))
 }
 
 complete -F _pulse pulse
